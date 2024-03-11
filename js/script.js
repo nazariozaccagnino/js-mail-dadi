92f3bc638c7d751a9eefbe2da9ef7b802de0d10a
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-03-11 16:43:37 +0100

    game done with calculations and images

--- js/script.js
+++ js/script.js
@@ -16,12 +16,12 @@ const addresses = [
     "twoflower@me.com",
     "punkis@comcast.net"
  ];
-let mailButton = document.getElementById('mailTool');
-// mailButton.addEventListener('click', function(){)
+// let mailButton = document.getElementById('mailTool');
+// mailButton.addEventListener('click', function(){
+//     let mailapp = document.getElementById('mailapp').classList;
+//     mailapp.className('d-none')
 
-
-
-
+// })
 let checkMailButton = document.getElementById('checkMail')
 checkMailButton.addEventListener('click', function() {
     let elEmailInput = document.getElementById('emailInput');
@@ -33,9 +33,9 @@ checkMailButton.addEventListener('click', function() {
         console.log(addresses)
         if (emailInput === addresses[i]){
             console.log('ok')
-            message = `<div>OK</div>`
+            message = `<div>Login effettuato</div>`
         } else{
-            message = `<div>SBAGLIATO</div>`
+            message = `<div>E-mail errata</div>`
         }
         elMessage.innerHTML = message;
     } 
@@ -49,3 +49,37 @@ checkMailButton.addEventListener('click', function() {
 // Prima di partire a scrivere codice poniamoci qualche domanda:
 // Che ci sia un array da qualche parte?
 // Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+// let playerDice = Math.floor((Math.random() * 6) + 1 ) ;
+// console.log(playerDice);
+// let pcDice = Math.floor((Math.random() * 6) + 1 ) ;
+// console.log(pcDice);
+
+
+
+// for (let i = 1; i <= numbers.length; i++) {
+    
+    let playerDice = Math.floor((Math.random() * 6) + 1 ) ;
+    console.log(playerDice);
+    let pcDice = Math.floor((Math.random() * 6) + 1 ) ;
+    console.log(pcDice);
+
+    let dice1 = document.getElementById('img1');
+    dice1.setAttribute('src',`img/dices/${[pcDice]}.svg`)
+    let dice2 = document.getElementById('img2');
+    dice2.setAttribute('src',`img/dices/${[playerDice]}.svg`)
+    
+    let elResult = document.getElementById('result');
+    let tmpHtml = '';
+    if (pcDice === playerDice){
+        console.log('PAREGGIO')
+        tmpHtml = `<h1>Pareggio</h1>`
+    } else if (pcDice < playerDice) {
+        console.log('IL PC HA VINTO')
+        tmpHtml = `<h1>Il pc ha vinto</h1>`
+    } else{
+        console.log('L\'UTENTE HA VINTO')
+        tmpHtml = `<h1>l'utente ha vinto</h1>`
+    }
+    elResult.innerHTML = tmpHtml;
+// }
